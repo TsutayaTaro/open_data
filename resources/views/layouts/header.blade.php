b2288fdbfcd0e78c553bf6c8ca5a6c3a745a6a81
TsutayaTaro <zero2darkzero@yahoo.co.jp>
2018-12-18 22:35:41 +0900

    Changed navber color

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     </div>
 </div>
     
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:green;">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:lightgreen;">
     <a class="navbar-brand" href="#">KISAPON GO</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
